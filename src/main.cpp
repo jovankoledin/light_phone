5f628ead046325b4ec2c3c54e0f0bfcd78380a80
jovankoledin <jovank@uw.edu>
2025-08-23 01:44:27 -0700

    Added path

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ void ledWaveTask(void* pvParameters) {
 
           // The brightness is set to a consistent, medium-low value.
           // This prevents any LEDs from turning off and keeps the visual calm.
-          uint8_t brightness = 10;
+          uint8_t brightness = 15;
 
           leds[idx] = CHSV(hue, saturation, brightness);
         }
